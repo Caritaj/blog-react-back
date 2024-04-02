4c06e0b672d20a4904be92d85283b294a34a559c
Rue Ccarita <xdtaur013@gmail.com>
2024-04-02 23:58:33 +0200

    controller users methods

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,43 +1,170 @@
-// REGISTER A NEW USER
-// POST: api/users/register
-// UNPROTECTED
+const User = require('../models/userModel')
+const HttpError = require('../models/errorModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const fs = require('fs');
+const path = require('path');
+const { v4: uuid } = require('uuid');
+
+
+/* 
+    Register a new user
+    Method: POST
+    Route: api/users/register
+    Protected: No
+*/
 const registerUser = async (req, res, next) => {
-    res.json('Register User')
-}
+    try {
+        const { name, email, password, password2 } = req.body;
+        if (!name || !email || !password || !password2) {
+            return next(new HttpError('Fill in all fields', 422));
+        }
 
-// LOGIN A NEW USER
-// POST: api/users/login
-// UNPROTECTED
+        const newEmail = email.toLowerCase();
+        const emailExists = await User.findOne({ email: newEmail })
+
+        if (emailExists) {
+            return next(new HttpError('Email already exits', 422));
+        }
+
+        if (password.length < 6) {
+            return next(new HttpError('Password should be at least 6 characters', 422));
+        }
+
+        if (password !== password2) {
+            return next(new HttpError('Passwords do not match', 422));
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPass = await bcrypt.hash(password, salt);
+        const newUser = await User.create({ name, email: newEmail, password: hashedPass })
+        res.status(201).json(`New User ${newUser.email} registered`)
+
+    } catch (error) {
+        return next(new HttpError('User registration failed', 422));
+    }
+}
+/* 
+    Login a new user
+    Method: POST
+    Route: api/users/login
+    Protected: No
+*/
 const loginUser = async (req, res, next) => {
-    res.json('Login User')
-}
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return next(new HttpError('Fill in all fields', 422));
+        }
 
-// USER PROFILE
-// POST: api/users/:id
-// PROTECTED
+        const newEmail = email.toLowerCase();
+        const user = await User.findOne({ email: newEmail })
+        const comparePass = await bcrypt.compare(password, user.password);
+        if (!comparePass) {
+            return next(new HttpError('Invalid credentials', 422));
+        }
+
+        if (!user) {
+            return next(new HttpError('Invalid credentials', 422));
+        }
+
+        const { _id: id, name } = user;
+        const token = jwt.sign({ id, name }, process.env.JWT_SECRET, { expiresIn: '1h' })
+        res.status(200).json({ token, id, name })
+
+    } catch (error) {
+        return next(new HttpError('Login registration failed', 422));
+    }
+}
+/* 
+    Get user profile
+    Method: POST
+    Route: api/users/:id
+    Protected: Yes
+*/
 const getUser = async (req, res, next) => {
-    res.json('User profile')
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id).select('-password');
+        if (!user) {
+            return next(new HttpError('User not found', 404));
+        }
+        res.status(200).json(user)
+    } catch (error) {
+        return next(new HttpError(error));
+    }
 }
-
-// CHANGE USER AVATAR (profile picture)
-// POST: api/users/change-avatar
-// PROTECTED
+/* 
+    Change user avatar(profile picture)
+    Method: POST
+    Route: api/users/change-avatar
+    Protected: Yes
+*/
 const changeAvatar = async (req, res, next) => {
-    res.json('Change User Avatar')
-}
+    try {
+        if (!req.files.avatar) {
+            return next(new HttpError('No avatar selected', 422));
+        }
 
-// EDIT USER DETAILS (from profile)
-// POST: api/users/edit-user
-// PROTECTED
+        //find user from database
+        const user = await User.findById(req.user.id);
+        //delete old avatar if exists
+        if (user.avatar) {
+            fs.unlink(path.join(__dirname, '..', 'uploads', user.avatar), (err) => {
+                if (err) {
+                    return next(new HttpError(err));
+                }
+            })
+        }
+
+        const { avatar } = req.files;
+        //check file size
+        if (avatar.size > 500000) {
+            return next(new HttpError('Profile picture too big. Should be less than 500kb', 422));
+        }
+
+        let fileName;
+        fileName = avatar.name;
+        let splittedFilename = fileName.split('.');
+        let newFilename = splittedFilename[0] + uuid() + '.' + splittedFilename[splittedFilename.length - 1];
+        avatar.mv(path.join(__dirname, '..', 'uploads', newFilename), async (err) => {
+            if (err) {
+                return next(new HttpError(err));
+            }
+        })
+
+        const updatedAvatar = await User.findByIdAndUpdate(req.user.id, { avatar: newFilename }, { new: true });
+        if (!updatedAvatar) {
+            return next(new HttpError('Avatar could not be changed', 422));
+        }
+        res.status(200).json(updatedAvatar)
+
+    } catch (error) {
+        return next(new HttpError(error));
+    }
+}
+/* 
+    Edit user details(from profile)
+    Method: PUT
+    Route: api/users/edit-user
+    Protected: Yes
+*/
 const editUser = async (req, res, next) => {
     res.json('Edit user details')
 }
-
-// GET AUTHORS
-// POST: api/users/authors
-// UNPROTECTED
+/* 
+    Get authors
+    Method: GET
+    Route: api/users/authors
+    Protected: No
+*/
 const getAuthors = async (req, res, next) => {
-    res.json('Get all users/authors')
+    try {
+        const authors = await User.find().select('-password');
+        res.json(authors)
+    } catch (error) {
+        return next(new HttpError(error));
+    }
 }
 
 module.exports = {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 const { registerUser, loginUser, getUser, changeAvatar, editUser, getAuthors } = require('../controllers/userControllers')
-
+const authMiddleware = require('../middleware/authMiddleware')
 
 const router = Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/:id', getUser);
-router.put('/change-avatar', changeAvatar);
+router.post('/change-avatar', authMiddleware, changeAvatar);
 router.put('/edit-user', editUser);
 router.get('/', getAuthors);
 
